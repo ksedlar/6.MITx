4749cd274dd13794eb5171277479cea66c00a6c2
ksedlar <katiesedlar@31-34-109.wireless.csail.mit.edu>
2014-06-17 14:14:47 -0400

    made items switch locations

--- Knapsack/Knapsack.js
+++ Knapsack/Knapsack.js
@@ -1,15 +1,60 @@
-function switchLocation(){
-        
+function moveElement($elem, amount, axis){
+    //finds the initial location of the element
+    var startpoint = $elem.position()[axis];
+    //computes the point the element will end at
+    var endpoint = startpoint + amount;
+    //This is basically a fancy way of saying animate(axis: endpoint + "px") in a syntactically correct way
+    var css = {};
+    css[axis] = endpoint + "px";
+    $elem.animate(css);
+    
 };
+
+function switchLocation(obj){
+    // if  (obj.parent == $('#house'))
+    console.log(obj);
+    console.log(obj.parent());
+    console.log($('#house'));
+    console.log(obj.parent().attr('id'));
+    if (obj.parent().attr('id') == 'house'){
+        console.log("yay1111");
+        obj.detach();
+        $('#sack').append(obj);
+    }
+    else if (obj.parent().attr('id') == 'sack'){
+        console.log("yay1111");
+        obj.detach();
+        $('#house').append(obj);
+    }
+    /*
+    if (){
+        
+        obj.attr(place) = "sack";
+    }
+    else if (obj.attr(place) == "sack"){
+        obj.attr(place) = "house";
+        moveElement(obj, -325, "left");
+    }
+    */
+};
+
 
 $(function(){
 //    var $items = $('#location *');
     var items = [];
     items = $('.item');
-    for (i = 0; i < items.length; i++){      
+    for (var i = 0; i < items.length; i++){      
         var text = ("$" + $(items[i]).data('value') + ", " + $(items[i]).data('weight') + "kg");
         $(items[i]).append($('<img src=' + $(items[i]).data('name') + '.png>'));
         $(items[i]).append($('<div>' + text +'</div>'));
+        console.log(items[i]);
     }
+    
+    
+    $('.item').click(function() {
+        console.log("blah");
+    
+        switchLocation($(this));
+    });
 });
 
